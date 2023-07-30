b63fa90a4a2b031c2ecfdbc3941f78c9fa835761
Dominik Courcelles <dominik.courcelles@gmail.com>
2023-07-30 17:50:04 -0400

    Forget the keyword await for the DefaultHandler when adding an attachment.

--- apps/meteor/app/file-upload/server/lib/DefaultHandler.ts
+++ apps/meteor/app/file-upload/server/lib/DefaultHandler.ts
@@ -2,7 +2,7 @@ import { UploadHandler, PostFileData, PostFields } from './UploadHandler';
 
 export class DefaultHandler extends UploadHandler {
     public async processAttachment(file: PostFileData, fields: PostFields): Promise<object> {
-		const uploadedFile = this.insertIntoFileStore(file, fields);
+		const uploadedFile = await this.insertIntoFileStore(file, fields);
         return Meteor.call('sendFileMessage', this.rid, null, uploadedFile, fields);
     }
 }
